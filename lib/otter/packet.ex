cb02945908fc1e228c34c27ce2d282f17cb5614c
edustocchero <eduardostocchero@proton.me>
2023-08-23 09:38:36 -0300

    feat: add set and forget packets

--- lib/otter/packet.ex
+++ lib/otter/packet.ex
@@ -1,13 +1,45 @@
 defmodule Otter.Packet do
-  def handle_packet(<<42>>) do
+  @moduledoc """
+  This module transforms binary packets into elixir structures.
+  """
+
+  def handle_packet(<<42>>), do: handle_heartbeat()
+
+  def handle_packet(<<43, rest::binary>>), do: handle_get(rest)
+
+  def handle_packet(<<44, rest::binary>>), do: handle_set(rest)
+
+  def handle_packet(<<45, rest::binary>>), do: handle_forget(rest)
+
+  def handle_packet(_packet), do: error(:unexpected_packet)
+
+  defp handle_heartbeat() do
     IO.puts("Received a heartbeat")
     {:ok, :heartbeat}
   end
 
-  def handle_packet(<<43, rest::binary>>), do: handle_get(rest)
-
   defp handle_get(<<length::8, name::binary-size(length)>>) do
     IO.puts("Received a get '#{name}' request")
-    {:ok, name: name}
+    {:ok, [:get, name: name]}
+  end
+
+  def handle_set(<<length::8, name::binary-size(length), rest::binary>>) do
+    handle_set_payload(rest, [:set, name: name])
+  end
+
+  defp handle_set_payload(<<length::16, payload::binary-size(length)>>, props) do
+    {:ok, props ++ [payload: payload]}
+  end
+
+  defp handle_set_payload(_payload, _name), do: error(:unexpected_payload)
+
+  defp handle_forget(<<length::8, name::binary-size(length)>>) do
+    {:ok, [:forget, name: name]}
+  end
+
+  defp handle_forget(_payload), do: error(:unexpected_payload)
+
+  defp error(name) do
+    {:error, name}
   end
 end
